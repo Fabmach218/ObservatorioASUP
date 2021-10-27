fcf8f7e9358110935009b3a5e166f9daa54b47cc
JCJ05 <julitoaguerorengifo@hotmail.com>
2021-10-27 10:52:42 -0500

    Agregando Vistas Principal , Filtro y Login

---
+++ observatorioapp/Controllers/FiltroController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace observatorioapp.Controllers
+{
+    public class FiltroController : Controller 
+    {
+        
+      
+      public IActionResult Busqueda(){
+
+           return View();
+
+      }
+
+    }
+}
--- observatorioapp/Controllers/HomeController.cs
+++ observatorioapp/Controllers/HomeController.cs
@@ -23,6 +23,8 @@ namespace observatorioapp.Controllers
             return View();
         }
 
+        
+
         public IActionResult Privacy()
         {
             return View();
