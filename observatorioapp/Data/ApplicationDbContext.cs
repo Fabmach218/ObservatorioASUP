d9ade4b548ae35c274a85d9f614968e2be1c7494
Fabio Marangon <fabio_marangon@usmp.pe>
2021-10-27 13:15:47 -0500

    Agregando contacto

--- observatorioapp/Data/ApplicationDbContext.cs
+++ observatorioapp/Data/ApplicationDbContext.cs
@@ -16,6 +16,7 @@ namespace observatorioapp.Data
          public DbSet<observatorioapp.Models.Usuario> DataUsuarios {get; set; }
         public DbSet<observatorioapp.Models.Noticia> DataNoticias {get; set; }
         public DbSet<observatorioapp.Models.Universidad> DataUniversidades {get; set; }
+        public DbSet<observatorioapp.Models.Contacto> DataContactos {get; set; }
 
     }
 }
---
+++ observatorioapp/Data/Migrations/20211027181040_ContactoMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace observatorioapp.Data.Migrations
+{
+    public partial class ContactoMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "t_contacto",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Correo = table.Column<string>(type: "text", nullable: false),
+                    Celular = table.Column<int>(type: "integer", nullable: false),
+                    Mensaje = table.Column<string>(type: "text", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_contacto", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "t_contacto");
+        }
+    }
+}
