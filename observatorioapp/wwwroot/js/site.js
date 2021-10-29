e39db9edb848ae6df3eb0d07aa04b58e457e1de2
JCJ05 <julitoaguerorengifo@hotmail.com>
2021-10-28 20:35:47 -0500

    Agregando Vistas de Confirmacion y codigo Js

--- observatorioapp/wwwroot/js/site.js
+++ observatorioapp/wwwroot/js/site.js
@@ -2,3 +2,56 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+let count = 6;
+let porcentaje = 0;
+ function redireccion(){
+
+   
+   
+    if(window.location.pathname === "/Identity/Account/ConfirmEmail"){
+
+        mensaje();
+        setInterval(mensaje , 1000);
+        setTimeout("redirect()", 5000);
+
+    }
+
+}
+
+
+
+function redirect(){
+
+    window.location.href = "https://localhost:5001/Identity/Account/Login";
+
+  }
+
+function mensaje(){
+
+    const texto = document.querySelector('#mensaje');
+    let barrita = document.querySelector('.progress-bar');
+    count--;
+ 
+    if(count < 0){
+
+        count = 0;
+    }
+    
+    if(porcentaje > 100){
+
+        porcentaje = 100;
+
+    }
+    
+    texto.textContent = "Su correo a sido confirmado se le redireccionara a la pagina de inicio de sesión en: " + count + " segundos";
+    barrita.ariaValueNow = porcentaje;
+    barrita.innerHTML = porcentaje + "%";
+    barrita.style.width = porcentaje + "%";
+    porcentaje+= 25;
+    
+
+    
+
+}
