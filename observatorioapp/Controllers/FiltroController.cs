a80f52c9436998f9940cc7b881610e9119e582b8
JCJ05 <julitoaguerorengifo@hotmail.com>
2021-11-01 20:46:03 -0500

    Agregando Normativas a vista de usuario final

--- observatorioapp/Controllers/FiltroController.cs
+++ observatorioapp/Controllers/FiltroController.cs
@@ -1,16 +1,34 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using observatorioapp.Data;
 
 namespace observatorioapp.Controllers
 {
     public class FiltroController : Controller 
     {
-        
+
+      private readonly ApplicationDbContext _context; 
+
+      public FiltroController(ApplicationDbContext context)
+      {
+          _context = context;
+      } 
       
       public IActionResult Busqueda(){
-
-           return View();
+          
+           var normativas = _context.DataNormativas.Include( e => e.entidad).ToList();
+           return View(normativas);
 
       }
+
+       public IActionResult DownLoadNormativa(int id){
+        
+           var normativas = _context.DataNormativas.Find(id);
+           return File(normativas.archivo, "application/pdf", normativas.nombrefile);  
+          
+
+       }
 
     }
 }
